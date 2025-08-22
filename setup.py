73997f2fea1df305c7f3191203cf6081ca538088
MNofil <nofilsiddiqui300@gmail.com>
2025-08-22 08:46:31 -0700

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="medical-chatbot",
+    version="0.1.0",
+    author="Muhammad Nofil Bin Mehar",
+    author_email="nofilsiddiqui300@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
